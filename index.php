b2ef21cfe556bdb30e7535f4aac4fed5aefb1b0f
mssusee <42839930+mssusee@users.noreply.github.com>
2020-04-20 16:48:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
-<title> ALB DEMO </title>
+<title> ALB Test Checking </title>
 <body>
 <h1>
 <center>
-<IMG SRC="Rose.jpg" ALT="omg"><br>
+<IMG SRC="Rose.jpg" ALT="This is Website"><br>
 <?php
 $eip = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
 echo $eip;
