b6230de128af2180e523cdb3a31d16f12bda25f0
mssusee <42839930+mssusee@users.noreply.github.com>
2020-04-20 11:57:11 +0530

    Create web1.sh

---
+++ web1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+yum install -y httpd php git
+cd /var/www/html
+wget https://github.com/mssusee/ALB/blob/master/Rose.jpg
+wget https://github.com/mssusee/ALB/blob/master/index.php
+wget https://github.com/mssusee/ALB/blob/master/htaccess
+mv /var/www/html/htaccess /var/www/html/.htaccess
+
+mkdir /var/www/html/Rose
+cp /var/www/html/Rose.jpg /var/www/html/Rose
+cp /var/www/html/index.php /var/www/html/Rose
+cp /var/www/html/.htaccess /var/www/html/Rose
+
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
